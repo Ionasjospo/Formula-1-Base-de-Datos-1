d1a9e27cc3cb7eca6b5a45d3394872b42514999a
ionasjospo <ionas.josponis@correo.ucu.edu.uy>
2023-05-30 14:57:25 -0300

    First commit

---
+++ Formula1/src/main/java/bd1/formula1/Formula1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package bd1.formula1;
+
+/**
+ *
+ * @author Ionas
+ */
+public class Formula1 {
+
+    public static void main(String[] args) {
+        String aQue = "blunt";
+        System.out.println("Mami dale mecha a ese " + aQue);
+    }
+}
